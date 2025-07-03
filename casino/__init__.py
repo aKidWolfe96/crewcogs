8c4067ae05d8455231a97cf411e7306322607c7e
aKidWolfe96 <akidwolfe@gmail.com>
2025-07-02 20:22:37 -0400

    Update __init__.py

--- casino/__init__.py
+++ casino/__init__.py
@@ -1,8 +1,10 @@
 from .blackjack import Blackjack
 from .coinflip import CoinFlip
 from .leaderboard import CasinoLeaderboard
+from .dailyspin import DailySpin
 
 async def setup(bot):
     await bot.add_cog(Blackjack())
     await bot.add_cog(CoinFlip(bot))
     await bot.add_cog(CasinoLeaderboard(bot))
+    await bot.add_cog(DailySpin(bot))
