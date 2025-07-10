d475feb4c91aad1544a7ecd3b60cc458ceeb21fa
aKidWolfe96 <akidwolfe@gmail.com>
2025-07-10 10:34:03 -0400

    Update __init__.py

--- plex/__init__.py
+++ plex/__init__.py
@@ -1,7 +1,6 @@
-from redbot.core import bot, commands
 from .plex import Plex
 
 __red_end_user_data_statement__ = "This cog does not store any end user data."
 
-def setup(bot: bot.Red):
-    bot.add_cog(Plex(bot))
+async def setup(bot):
+    await bot.add_cog(Plex(bot))
