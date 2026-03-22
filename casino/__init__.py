d0aa9d596ffd2f11a1ef9a577a6de0840c6771cf
aKidWolfe96 <akidwolfe@gmail.com>
2026-03-22 00:04:31 -0400

    Add HorseRace cog to casino setup

--- casino/__init__.py
+++ casino/__init__.py
@@ -2,9 +2,11 @@ from .blackjack import Blackjack
 from .coinflip import CoinFlip
 from .leaderboard import CasinoLeaderboard
 from .dailyspin import DailySpin
+from .horserace import HorseRace
 
 async def setup(bot):
     await bot.add_cog(Blackjack())
     await bot.add_cog(CoinFlip(bot))
     await bot.add_cog(CasinoLeaderboard(bot))
     await bot.add_cog(DailySpin(bot))
+    await bot.add_cog(HorseRace(bot))
