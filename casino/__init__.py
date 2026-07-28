ee815aa12aeff6320e891d004d739ba7cb2b563b
aKidWolfe96 <akidwolfe@gmail.com>
2026-07-28 11:58:55 -0400

    Refactor imports and update bot setup function
    Reorganize imports and update setup function to include Slots.

--- casino/__init__.py
+++ casino/__init__.py
@@ -2,7 +2,6 @@ from .blackjack import Blackjack
 from .coinflip import CoinFlip
 from .leaderboard import CasinoLeaderboard
 from .dailyspin import DailySpin
-from .horserace import HorseRace
 from .slots import Slots
 from .roulette import Roulette
 
@@ -11,6 +10,5 @@ async def setup(bot):
     await bot.add_cog(CoinFlip(bot))
     await bot.add_cog(CasinoLeaderboard(bot))
     await bot.add_cog(DailySpin(bot))
-    await bot.add_cog(HorseRace(bot))
     await bot.add_cog(Slots(bot))
     await bot.add_cog(Roulette(bot))
